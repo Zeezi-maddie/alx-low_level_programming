cda456083ccd63237273db9e41b0c88411f4a84c
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-17 17:16:53 -0800

    Task 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char no = 'a';
+	while (no <= 'z')
+	{
+		if (no != 'e'&& no != 'q')
+		{
+			putchar(no);
+		}
+		no++;
+	}
+	putchar('\n');
+	return (0);
+}
