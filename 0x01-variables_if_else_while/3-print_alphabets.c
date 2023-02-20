053998ca25ab1bcf5da832ea7ecc838a0718c953
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-20 13:03:37 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
 	{
-		putchar(alp[i]);
+		putchar(alphabet[i]);
 	}
 	putchar('\n');
 	return (0);
