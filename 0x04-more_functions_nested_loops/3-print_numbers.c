c0e58f787e7330c2c34e93398a8a97828f5edc19
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-24 15:47:05 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 	for (b = 0; b <= 9; b++)
 	{
-		_putchar(b);
+		_putchar(b + '0');
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ *
