e3b4a514903fbf8c43139b0bf7ec3ce99e476a6e
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-24 16:00:28 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,4 +2,20 @@
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9
- *
+ *@2 - first integer to not print
+ *@4 - second integer to not print
+ *Return: 0 - 9 excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		if (b != 2 && b != 4)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}
