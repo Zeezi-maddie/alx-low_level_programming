efc0954d82bd359e7bb4f3b26a95d6915028b0c7
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-24 19:10:04 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,9 @@ void print_line(int n)
 {
 	int m;
 
-	if (n <= 0)
+	for (m = 1; m < n; m++)
 	{
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (m = 1; m <= n; m++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
