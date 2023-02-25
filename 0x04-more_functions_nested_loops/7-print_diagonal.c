0d041cf2679c9bd3b0eedec6c4198b6b8f6dc2ce
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-24 17:11:35 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n - number of times the character \ should be printed
+ * Return: diagonal line on the terminal
+ */
+void print_diagonal(int n)
+{
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square -  prints a square
+ * only use _putchar function to print
+ * size - the size of the square
+ * Return: character # to print the square
+ */
+void print_square(int size)
+{
+	int side1;
+	int side2;
+
+	if (size <= 0)
+	{
+		_putchar('\n);
+	}
+	else
+	{
+		for (side1 = 1; side1 <= size; side1++)
+		{
+			_putchar('#');
+		for (side2 = 2; side2 <= size; side2++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		}
+	}
+}
+
