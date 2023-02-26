9bdd90e68ac7a8dd7224aa4b1da9517ed9308dd8
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-02-26 14:40:43 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,12 @@ int print_last_digit(int e)
 	d = (e % 10);
 	if (d < 0)
 	{
-		_putchar(-d + 0);
+		_putchar(-d + 48);
 		return (-d);
 	}
 	else
 	{
-		_putchar(d + 0);
+		_putchar(d + 48);
 		return (d);
 	}
 }
