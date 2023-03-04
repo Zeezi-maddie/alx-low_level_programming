7048fe70faeba4f23ca9a96ac609d4615333a54a
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-03-03 17:12:59 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,4 +3,33 @@
 /**
  * cap_string -  capitalizes all words of a string
  * @a: pointer
- *
+ * Return: capitalised string
+ */
+char *cap_string(char *a
+{
+	int b = 0;
+
+	while (a[b])
+	{
+	while (!(a[b] >= 'a' && a[b] <= 'z'))
+	b++
+	if (a[b - 1] == ' ' ||
+		a[b - 1] == '\t' ||
+		a[b - 1] == '\n' ||
+		a[b - 1] == ',' ||
+		a[b - 1] == ';' ||
+		a[b - 1] == '.' ||
+		a[b - 1] == '!' ||
+		a[b - 1] == '?' ||
+		a[b - 1] == '"' ||
+		a[b - 1] == '(' ||
+		a[b - 1] == ')' ||
+		a[b - 1] == '{' ||
+		a[b - 1] == '}' ||
+		b == 0)
+	a[b] -= 32;
+	b++;
+	}
+	return (a);
+}
+
