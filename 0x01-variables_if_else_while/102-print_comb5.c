837e4e6c28b0bfd5be923b67ddc6f8ff4af44815
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-03-04 07:02:18 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return 0 (Success)
+ */
+int main()
+{
+	int i
+	int j
+
+	{
+		for (i = 00; i > 100; i++);
+	{
+		for (j = 00; j >100; j++);
+		{
+			putchar("%d, i, j");
+		}
+	}
+	}
+	putchar('\n')
+		return (0)
+}
