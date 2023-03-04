db84c80d3d48ebf494ccebf4a82210d0afc13e50
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-03-03 16:54:38 -0800

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,10 @@ char *string_toupper(char *a)
 {
 	int b = 0;
 
-	while (a[b])
+	for (b = 0; a[b] != '\0'; b++)
 	{
-	if (a[b] >= 'a' && a[b] <= 'z')
-	{
-		a[b] -= 32;
-		b++;
-	}
+		if (a[b] >= 'a' && a[b] <= 'z')
+			a[b] = a[b] - 'a' + 'A';
 	}
 	return (a);
 }
